84cde43abc5ca63a9ac08e74bdf7441a144d52ca
GianmauroCoviello <gianmaurocoviello@gmail.com>
2023-06-22 02:17:51 +0200

    [DOCS] Progetto completato.

--- src/components/AppJumbotron.vue
+++ src/components/AppJumbotron.vue
@@ -9,7 +9,11 @@ export default {
 <template lang="">
     <!-- container che conterra il jumbotron -->
     <div class="container-jumbotron">
-        <button><h2>CURRENT SERIES</h2></button>
+        <div class="row">
+            <!-- button che avrà la proprietà position per posizionarsi -->
+            <button><h2>CURRENT SERIES</h2></button>
+        </div>
+        
     </div>
 </template>
 
@@ -21,8 +25,12 @@ export default {
         padding: 160px 0;
         position: relative;
 
+        .row{
+            max-width: 1170px;
+            margin: 0 auto;
+            
 
-        button{
+            button{
             padding: 10px 20px;
             background-color: rgb(46, 116, 255);
             border: none;
@@ -30,9 +38,11 @@ export default {
             font-weight: 900;
             position: absolute;
             top: 298px;
-            left: 80px;
             
+            
+            }
         }
+        
     }
     
 </style>
